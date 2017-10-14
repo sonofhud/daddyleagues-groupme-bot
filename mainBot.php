6109f4864fab392f055d2ddefdb9b7d12bc58845
sonofhud <32784331+sonofhud@users.noreply.github.com>
2017-10-13 20:09:35 -0500

    Update mainBot.php

--- mainBot.php
+++ mainBot.php
@@ -6,7 +6,7 @@ include('./httpful.phar');
 include('./functions.php');
 
 // this is found at http://dev.groupme.com/bots
-$bot_token = "the main bot id";
+$bot_token = "1cc36cd1f812c86944c5046bc4";
 
 include('./index.php');
 
